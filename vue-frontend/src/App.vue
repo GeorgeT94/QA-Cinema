e86d69521ad1934cfbad98304464611f32cf9dce
Nawid Mujadidi <nawidmujadidi@Nawids-MacBook-Air.local>
2018-09-04 21:58:42 +0100

    added footer

--- vue-frontend/src/App.vue
+++ vue-frontend/src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <Navbar/>
     <Homepage/>
+    <Footer/>
   </div>
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue'
 import Homepage from './pages/Homepage.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'app',
   components: {
     Navbar,
-    Homepage
+    Homepage,
+    Footer
   }
 }
 </script>
