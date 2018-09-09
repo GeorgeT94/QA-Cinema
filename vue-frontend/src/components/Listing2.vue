ef9ea937c2610f21573a59dee6e881224e7b0805
Nawid Mujadidi <nawidmujadidi@Nawids-MacBook-Air.local>
2018-09-09 15:33:07 +0100

    updating MoviesPage

---
+++ vue-frontend/src/components/Listing2.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="listing">
+    <h3>{{title}}</h3>
+    <div class="singleMovie" v-for="movie in movieList">
+      <router-link to="/movieInfo">
+            <img src="../assets/Deadpool-poster.jpg"><br>
+            <a class="social-icon">{{movie.name}}</a>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Listing2',
+  props: {
+    title: String,
+    movieList: Array
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#listing{
+  text-align: left;
+  color: white;
+  padding-left: 15%;
+  padding-right: 15%;
+  padding-top: 2%;
+  padding-bottom: 2%;
+  background-color: navy;
+}
+.singleMovie{
+  display: inline-block;
+  margin-right: 2%;
+  margin-bottom: 2%;
+  text-align: center;
+}
+img{
+  width: 150px;
+}
+</style>
--- vue-frontend/src/pages/MoviesPage.vue
+++ vue-frontend/src/pages/MoviesPage.vue
@@ -1,22 +1,58 @@
 <template>
   <div>
     <h1>Movie Gallery</h1>
+    <Listing2 title="Whats on" v-bind:movieList="movieList"/>
+    <Listing2 title="Coming Soon" v-bind:movieList="movieList"/>
   </div>
 </template>
 
 <script>
+import Listing2 from '../components/Listing2.vue'
 
 export default {
   name: 'MoviesPage',
   data() {
     return {
-
+      movieList: [
+        {
+          name: "Deadpool"
+        },
+        {
+          name: "Avengers"
+        },
+        {
+          name: "Deadpool"
+        },
+        {
+          name: "Deadpool"
+        },
+        {
+          name: "Deadpool"
+        },
+        {
+          name: "Deadpool"
+        },
+        {
+          name: "Deadpool"
+        },
+        {
+          name: "Deadpool"
+        },
+      ]
     }
+  },
+  components: {
+    Listing2
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+h1{
+  margin-left: 15%;
+  text-decoration: underline;
+  margin-top: 2%;
+  margin-bottom: 2%;
+}
 </style>
